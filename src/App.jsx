0ced913671dfc9a520809657d895f28cca35c0f7
joao-hideki <joaoohideki12345@gmail.com>
2023-01-20 18:02:01 -0300

    criando o componente Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './global.css';
+import { Header } from './components/Header';
 
 export function App() {
 
   return (
-    <div>Hello World</div>
+    <>
+      <Header/>
+
+    </>
   );
 }
 
