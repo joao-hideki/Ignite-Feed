15e344dcfba479dcd034b659245309ebd7bf627c
joao-hideki <joaoohideki12345@gmail.com>
2023-01-20 18:31:23 -0300

    criando o componente sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import './global.css';
+import styles from './App.module.css';
 import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
 
   return (
     <>
-      <Header/>
-
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main></main>
+      </div>
     </>
   );
 }
