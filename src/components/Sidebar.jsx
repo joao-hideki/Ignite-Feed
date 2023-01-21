561f4fbc614ee2ff65d0e48e4b352e8345f9bbf0
joao-hideki <joaoohideki12345@gmail.com>
2023-01-21 00:01:07 -0300

    finalizando a sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
-import styles from './Sidebar.module.css'
+import styles from './Sidebar.module.css';
+import {PencilLine} from 'phosphor-react';
 
 export function Sidebar() {
     return(
@@ -8,11 +9,13 @@ export function Sidebar() {
                 src="https://images.unsplash.com/photo-1457449940276-e8deed18bfff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
             />
             <div className={styles.profile}>
+                <img src='https://gcavocats.ca/wp-content/uploads/2018/09/man-avatar-icon-flat-vector-19152370-1.jpg' />
                 <strong>User Name</strong>
                 <span>Profession</span>
             </div>
             <footer>
                 <a href='#'>
+                    <PencilLine size={20} />
                     Editar seu perfil
                 </a>
             </footer>
